fd4066f7cb2bc31cc1b9e8658323155acef67327
AniaChil <43175425+AniaChil@users.noreply.github.com>
2018-09-13 18:23:28 +0200

    Initial commit

---
+++ hello.py
@@ -0,0 +1,12 @@
+#komentarz
+
+print("Hello world!")
+print('Hello "world"!')
+print("3")
+print(3)
+print(type("3"))
+print(type(3))
+print(type(3.14))
+print(0.1+0.1+0.1)
+print(type(True))
+print(type(None))
