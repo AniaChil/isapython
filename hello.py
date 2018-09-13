394e6df11d6f5851c35a9ac6265e0cc41a353bdf
AniaChil <43175425+AniaChil@users.noreply.github.com>
2018-09-13 19:13:17 +0200

    commit

--- hello.py
+++ hello.py
@@ -2,11 +2,30 @@
 
 print("Hello world!")
 print('Hello "world"!')
-print("3")
-print(3)
-print(type("3"))
-print(type(3))
+
+number = 3
+number_str = "3"
+
+print(number_str)
+print(number)
+print(type(number_str))
+print(type(number))
 print(type(3.14))
 print(0.1+0.1+0.1)
 print(type(True))
 print(type(None))
+
+number = number + 1
+print(number)
+
+hello = "Hello"
+world = "world"
+ex = "!"
+
+#konkatenacja stringów
+sign = hello +" "+ world + ex
+print(sign)
+
+name = input ("Jak masz na imię?")
+print(hello + " " + name + ex)
+
